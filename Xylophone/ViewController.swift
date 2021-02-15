2e9814d0c00b0a76cd39925ef6fe0b4bb850ba98
Norberto Studios <norbertostudios@gmail.com>
2021-02-14 22:03:14 -0500

    challenge completed

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,37 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        let soundTitle = sender.currentTitle
+        let secondsToDelay = 0.2
+//        print(soundTitle!)
+//  The '!' work the same 'as String'
+        playSound(sound: soundTitle!)
+        
+         sender.alpha = 0.5
+    DispatchQueue.main.asyncAfter(deadline: .now() + secondsToDelay) {
+        sender.alpha = 1
+        }
+//
+       
+    }
     
+    func playSound(sound: String) {
+        let url = Bundle.main.url(forResource: sound, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 
 }
 
